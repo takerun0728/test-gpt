54c152e564273c6f9c8fabe9fc9673beb04b0793
Takeru Goto <takeru_goto@jp.honda>
2025-01-07 19:11:58 +0900

    Update simple_lstm.py

--- simple_lstm.py
+++ simple_lstm.py
@@ -1,4 +1,33 @@
 import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import dataset, dataloader
+
+class SimpleLSTM(nn.Module):
+    def __init__(self, num_embedding, embedding_dim, hidden_size):
+        super(SimpleLSTM, self).__init__()
+        self.hidden_size = hidden_size
+        self.embedding = nn.Embedding(num_embedding, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_size, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_embedding)
+        self.output = nn.Softmax(dim=1)
+
+    def forward(self, x, hidden=None):
+        x = self.embedding(x)
+        x, hidden = self.lstm(x, hidden)
+        x = self.fc(x)
+        return self.output(x)
+
+class TrainDataset(dataset.Dataset):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __len__(self):
+        return len(self.y)
+    
+    def __getitem__(self, index):
+        return self.x[index], self.y[index]
 
 text_data = """むかしむかしあるところに、
 お爺さんとお婆さんがいました。
@@ -12,7 +41,17 @@ decoded = [int_to_char[i] for i in encoded]
 seq_length = len(text_data.split('\n')[0])
 sequences = [encoded[i:i+seq_length+1] for i in range(len(encoded) -seq_length)]
 
-X = torch.Tensor([seq[:-1] for seq in sequences])
-Y = torch.Tensor([seq[1:] for seq in sequences])
+X = torch.tensor([seq[:-1] for seq in sequences], dtype=torch.int)
+Y = torch.tensor([seq[1:] for seq in sequences], dtype=torch.int)
+
+model = SimpleLSTM(len(char_to_int), 10, 10)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+data = dataset.TensorDataset(X, Y)
+loader = dataloader.DataLoader(dataset, batch_size=64, shuffle=True)
+
+epoch = 10
+for i in range(epoch):
+    predict = loader.
 
 pass
